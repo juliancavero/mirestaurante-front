7a5dc5f6913cf87ac594a9aa7fe615bd39cd5aae
Julian Cavero <juliancavero95@gmail.com>
2022-05-02 21:46:16 +0200

    main menu changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
 import { Camarero } from './components/camarero';
 import { Manager } from './components/manager';
 import { Boss } from './components/boss';
