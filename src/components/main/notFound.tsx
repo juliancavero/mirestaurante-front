b1e481f9091d24535bf5e5420e465788dd8064bd
juliancavero <juliancavero95@gmail.com>
2022-06-10 19:15:47 +0200

    last

--- src/components/main/notFound.tsx
+++ src/components/main/notFound.tsx
@@ -3,7 +3,7 @@ export function NotFound() {
     <div className="container d-flex justify-content-center mt-3">
       <div className="row">
         <h1>Vaya...</h1>
-        <img src="images/404notfound.png" alt="404 NOT FOUND" />
+        <img src="/images/404notfound.png" alt="404 NOT FOUND" />
       </div>
     </div>
   );
